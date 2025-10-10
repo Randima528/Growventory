54509396435eaae8e6d7c86e389956a2f160b3f3
Randima528 <randimanethmini528@gmail.com>
2025-10-10 23:58:26 +0530

    Initialized Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import {Routes,Route, Navigate} from "react-router-dom"
+import Home from "./pages/Home";
+
+
+const App = () => {
+  return (
+    
+      
+      <Routes>
+
+         <Route path="/" element={<Home />} />
+      </Routes>
+    
+  )
+}
+
+    
+export default App
