d207b2d5654bd62f9c40023351154ff8d40ce068
Randima528 <randimanethmini528@gmail.com>
2025-10-15 17:28:32 +0530

    Add MySQL connection setup and update package dependencies

---
+++ Growventory-Backend/db.js
@@ -0,0 +1,19 @@
+import { createConnection } from "mysql2";
+
+const connection = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "inventory_db",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
+
+export default connection;
+
